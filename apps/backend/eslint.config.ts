5d40c75d86d09b79b871fabc8322ecac9ff867b0
TheBaconStyle <maks.christmas2001@gmail.com>
2025-11-17 01:24:42 +0200

    build(frontend & backend): changes linting & ts configs

--- apps/backend/eslint.config.ts
+++ apps/backend/eslint.config.ts
@@ -1,15 +1,7 @@
-import { defineConfig, globalIgnores } from 'eslint/config';
-import typescriptParser from '@typescript-eslint/parser';
 import typescriptPlugin from '@typescript-eslint/eslint-plugin';
-import js from '@eslint/js';
+import typescriptParser from '@typescript-eslint/parser';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
-import { FlatCompat } from '@eslint/eslintrc';
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
 
 export default defineConfig([
   {
@@ -28,19 +20,22 @@ export default defineConfig([
     plugins: {
       '@typescript-eslint': typescriptPlugin as any,
     },
-    extends: compat.extends(
+    extends: [
       'plugin:@typescript-eslint/recommended',
       'plugin:prettier/recommended',
-    ),
-
+    ],
     rules: {
       '@typescript-eslint/interface-name-prefix': 'off',
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars':'off'
+      '@typescript-eslint/no-unused-vars': 'off',
     },
   },
 
-  globalIgnores(['**/.eslintrc.js', '**/nestia.config.ts', '**/eslint.config.ts']),
+  globalIgnores([
+    '**/.eslintrc.js',
+    '**/nestia.config.ts',
+    '**/eslint.config.ts',
+  ]),
 ]);
--- apps/frontend/eslint.config.ts
+++ apps/frontend/eslint.config.ts
@@ -1,24 +1,13 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import js from '@eslint/js';
 import { defineConfig } from 'eslint/config';
-import path from 'node:path';
-
-// const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
 
 export default defineConfig([
   {
-    extends: compat.extends('next/core-web-vitals', 'next/typescript'),
+    extends: ['next/core-web-vitals', 'next/typescript'],
 
     rules: {
       'react/no-children-prop': 'off',
       '@typescript-eslint/no-unused-vars': 'warn',
-      '@typescript-eslint/no-explicit-any': 'warn'
+      '@typescript-eslint/no-explicit-any': 'warn',
     },
     ignores: ['.next/**/*'],
   },
